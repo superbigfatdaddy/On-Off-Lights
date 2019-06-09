99c8f63c7f5d9b1d5f23bdd0813fb32e9eff6030
superbigfatdaddy <51535471+superbigfatdaddy@users.noreply.github.com>
2019-06-09 23:47:11 +0800

    Add files via upload

---
+++ action-corridor_lights.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python2
+from hermes_python.hermes import Hermes
+
+MQTT_ADDR= "192.168.1.132:1883"
+
+print("hello world")
+def subscribe_corridor_lights_callback(hermes,intent_message):
+    print("Parse intent : {}" .format(intent_message.intent.intent_name))
+
+with Hermes(MQTT_ADDR) as h:
+    h.subscribe_intent("On-Off-Lights",subscribe_corridor_lights_callback).start()
