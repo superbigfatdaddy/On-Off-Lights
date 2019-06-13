0cf687e24920b88c3341273baafb97f8124d5030
superbigfatdaddy <51535471+superbigfatdaddy@users.noreply.github.com>
2019-06-13 23:17:29 +0800

    Add files via upload

--- action-corridor_lights.py
+++ action-corridor_lights.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import ConfigParser
+import configparser
 from hermes_python.hermes import Hermes
 from hermes_python.ffi.utils import MqttOptions
 from hermes_python.ontology import *
@@ -10,7 +10,7 @@ import io
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI ="config.ini"
 
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
+class SnipsConfigParser(configparser.SafeConfigParser):
     def to_dict(self):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
 
@@ -20,20 +20,21 @@ def read_configuration_file(configuration_file):
             conf_parser = SnipsConfigParser()
             conf_parser.readfp(f)
             return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
+    except (IOError, configparser.Error) as e:
         return dict()
 
-def subcribe_intent_callback(hermes, intentMessage):
+def subscribe_intent_callback(hermes, intentMessage):
     conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes, intentMessage, conf)
+    action_wrapper(hermes, intentMessage,conf)
 
-def action_wrapper(hermes, intentMessage, conf):
+def action_wrapper(hermes, intentMessage,conf):
     result_sentence="hello world"
     current_session_id = intentMessage.session_id
-    hermes.publish_end_session(current_session_id, result_sentence)
+    hermes.publish_end_session(current_session_id,result_sentence)
 
 if __name__ =="__main__":
     mqtt_opts = MqttOptions()
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("{{intent_id}}", subscribe_intent_callback).start()
+        h.subscribe_intent("Superbigfatdaddy:On-Off-Lights",subscribe_intent_callback)\
+         .start()
 
