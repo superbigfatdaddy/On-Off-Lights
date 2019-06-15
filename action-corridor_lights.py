7d4d2914334c3fb5de9d253404415e2bffdc4b85
superbigfatdaddy <51535471+superbigfatdaddy@users.noreply.github.com>
2019-06-16 00:23:56 +0800

    Add files via upload

---
+++ action-corridor_lights.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import configparser
+from hermes_python.hermes import Hermes
+from hermes_python.ffi.utils import MqttOptions
+from hermes_python.ontology import *
+import io
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI ="config.ini"
+
+class SnipsConfigParser(configparser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, configparser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage,conf)
+
+def action_wrapper(hermes, intentMessage,conf):
+#    result_sentence = intentMessage.intent.intent_name    ###say out the intent name
+#    result_sentence = str(intentMessage.slots)            ###say something which i dun understand
+    result_sentence = str(intentMessage.slots.myslot.first())
+    print("{}".format(intentMessage.intent.intent_name))
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id,result_sentence)
+
+if __name__ =="__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("Superbigfatdaddy:On-Off-Lights",subscribe_intent_callback).start()
+
